883324780bd2be04a8227e116f39f3936d48761c
chathuribuddhi <chathuri.buddhi@gmail.com>
2017-10-07 22:43:41 +0530

    database schema modified

--- database/schema.sql
+++ database/schema.sql
@@ -4,66 +4,38 @@ CREATE TABLE category
   catName VARCHAR(200),
   catDescription VARCHAR(200)
 );
-CREATE TABLE user
-(
-  custId VARCHAR(100) DEFAULT '' PRIMARY KEY NOT NULL,
-  name VARCHAR(200),
-  address VARCHAR(200),
-  phone VARCHAR(20),
-  mail VARCHAR(100),
-  password VARCHAR(20)
-);
 CREATE TABLE inventory
 (
   productId VARCHAR(100) DEFAULT '' PRIMARY KEY NOT NULL,
-  qty INT(11),
-  CONSTRAINT inventory_ibfk_1 FOREIGN KEY (productId) REFERENCES product (productId)
+  qty INT(11)
 );
 CREATE TABLE orderDetail
 (
   orderId VARCHAR(100),
   productId VARCHAR(100),
-  qty INT(11),
-  CONSTRAINT orderDetail_ibfk_1 FOREIGN KEY (orderId) REFERENCES orders (orderId),
-  CONSTRAINT orderDetail_ibfk_2 FOREIGN KEY (productId) REFERENCES product (productId)
+  qty INT(11)
 );
 CREATE INDEX orderId ON orderDetail (orderId);
-CREATE INDEX productId ON orderDetail (productId);
 CREATE TABLE orders
 (
   orderId VARCHAR(100) DEFAULT '' PRIMARY KEY NOT NULL,
   custId VARCHAR(100),
-  paymentId VARCHAR(100),
-  orderDate DATE,
-  CONSTRAINT orders_ibfk_1 FOREIGN KEY (custId) REFERENCES user (custId),
-  CONSTRAINT orders_ibfk_2 FOREIGN KEY (paymentId) REFERENCES payment (paymentId)
+  tel VARCHAR(20),
+  address VARCHAR(500)
 );
 CREATE INDEX custId ON orders (custId);
-CREATE INDEX paymentId ON orders (paymentId);
-CREATE TABLE payment
-(
-  paymentId VARCHAR(100) DEFAULT '' PRIMARY KEY NOT NULL,
-  amount DOUBLE,
-  custId VARCHAR(100),
-  CONSTRAINT payment_ibfk_1 FOREIGN KEY (custId) REFERENCES user (custId)
-);
-CREATE INDEX custId ON payment (custId);
 CREATE TABLE product
 (
   productId VARCHAR(100) DEFAULT '' PRIMARY KEY NOT NULL,
   productName VARCHAR(200),
   productDescription VARCHAR(200),
   catId VARCHAR(100),
-  unitPrice DOUBLE,
-  CONSTRAINT product_ibfk_1 FOREIGN KEY (catId) REFERENCES category (catId)
+  unitPrice DOUBLE
 );
 CREATE INDEX catId ON product (catId);
-
-SELECT  * FROM category;
-
-SELECT * FROM product;
-
-DELETE product FROM product WHERE productId = "aaa";
-
-
-
+CREATE TABLE user
+(
+  username VARCHAR(200) DEFAULT '' PRIMARY KEY NOT NULL,
+  password VARCHAR(200) NOT NULL,
+  userrole VARCHAR(50) DEFAULT 'USER'
+);
